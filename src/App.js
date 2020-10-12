b2ab879c35b3d04cff3524d34d76c219b11dca32
sasor <s.leamsi99@gmail.com>
2020-10-12 10:05:34 -0400

    layout de la pagina con grid css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { APP_URL } from './env';
 
 function App() {
   return (
-    <div className="App">
-      // Todo
+    <div className="grid-container">
+      <header>
+        <a href={APP_URL}>React Shopping Cart</a>
+      </header>
+      <main>
+        Product list
+      </main>
+      <footer>
+        All right is reserved.
+      </footer>
     </div>
   );
 }
