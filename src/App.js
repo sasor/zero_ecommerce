aa653641bdfe44fd37e2cdd69b0ea682c70051cd
sasor <s.leamsi99@gmail.com>
2020-10-13 09:43:18 -0400

    filter component done
    filter products and sort products

--- src/App.js
+++ src/App.js
@@ -2,18 +2,63 @@ import React, { Component } from 'react';
 import { APP_URL } from './env';
 import data from './data.json';
 import Products from './components/Products';
+import Filter from './components/Filter';
 
 class App extends Component {
 
   state = {
-    products: []
+    products: [],
+    size: "",
+    sort: ""
   };
 
   componentDidMount() {
     this.setState({ products: data.products })
   }
 
+  filterAction = (e) => {
+    const { value } = e.target;
+    let newState = null;
+    if (value) {
+      newState = Object.assign(
+        {},
+        { size: value },
+        {
+          products: data.products.filter(product => product.availableSizes.includes(value))
+        }
+      )
+    } else {
+      newState = Object.assign({}, { size: "" }, { products: data.products })
+    }
+    this.setState(newState, _ => console.log(this.state))
+  }
+
+  sortAction = (e) => {
+    const { value } = e.target;
+    let compareF = null;
+    switch (value) {
+      case "lowest": {
+        compareF = (a, b) => a.price - b.price;
+        break;
+      }
+      case "highest": {
+        compareF = (a, b) => b.price - a.price;
+        break;
+      }
+      default: {
+        compareF = (a, b) => a._id - b._id;
+      }
+    }
+    this.setState(prevState => {
+      return {
+        sort: value,
+        products: prevState.products.slice().sort(compareF)
+      }
+    }, _ => console.log(this.state));
+  }
+
   render() {
+    const { products, size, sort } = this.state;
     return (
       <div className="grid-container" >
         <header>
@@ -22,7 +67,13 @@ class App extends Component {
         <main>
           <div className="content">
             <div className="main">
-              <Products products={this.state.products} />
+              <Filter
+                count={products.length}
+                size={size}
+                sort={sort}
+                filterProducts={this.filterAction}
+                sortProducts={this.sortAction} />
+              <Products products={products} />
             </div>
             <div className="sidebar">Cart items</div>
           </div>
---
+++ src/components/Filter.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+
+class Filter extends Component {
+
+  render() {
+    const { count, sort, size, filterProducts, sortProducts } = this.props;
+    return (
+      <div className="filter">
+        <div className="result-result">{count} Products</div>
+        <div className="filter-sort">{` Order `}
+          <select value={sort} onChange={sortProducts}>
+            <option>Latest</option>
+            <option value="lowest">Lowest</option>
+            <option value="highest">Highest</option>
+          </select>
+        </div>
+        <div className="filter-size">{` Filter `}
+          <select value={size} onChange={filterProducts}>
+            <option value="">ALL</option>
+            <option value="XS">XS</option>
+            <option value="S">S</option>
+            <option value="M">M</option>
+            <option value="L">L</option>
+            <option value="XL">XL</option>
+            <option value="XXL">XXL</option>
+          </select>
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default Filter;
